a3ec57ee5017b7151673aa5f6d1034decb770522
Tranum Kamra <tranumkamra29@gmail.com>
2025-06-17 03:00:06 +0530

    updated codes

--- script.js
+++ script.js
@@ -35,19 +35,42 @@ function validateUsername(username){
 }
 
 async function fetchUserDetails(username){
-    const url=`https://leetcode-stats-api.herokuapp.com/${username}}`
     try{
         searchButton.textContent ="Searching... "
         searchButton.disabled=true;
 
-        const response = await fetch(url);
+        // const response = await fetch(url);
+        // const proxyUrl=`https://cors-anywhere.herokuapp.com/`;
+
+        // const targetUrl=`https://leetcode.com/graphql/`;
+        const proxyUrl = `https://cors-anywhere.herokuapp.com/https://leetcode.com/graphql/`;
+    const myHeaders=new Headers();
+    myHeaders.append("content-type","application/json");
+
+    const graphql=JSON.stringify({
+      query: "\n    query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n    count\n  }\n  matchedUser(username: $username) {\n    submitStats {\n      acSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n      totalSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n    }\n  }\n}\n    ",
+       variables: {"username": `${username}` }
+    })
+
+    const requestOptions={
+        method: "POST",
+        headers: myHeaders,
+        body: graphql,
+        redirect: "follow"
+
+
+    };
+
+    const response = await fetch(proxyUrl,requestOptions);
+
         if(!response.ok){
             throw new Error("Unable to fetch the User Details");
 
 
         }
-        const data= await response.json();
-        console.log("Logging data: ", data);
+        const parsedData= await response.json();
+        console.log("Logging data: ", parsedData);
+        displayUserData(parsedData);
     }
     catch(error){
         statsContainer.innerHTML= '<p>No data found</p>'
@@ -57,6 +80,34 @@ async function fetchUserDetails(username){
         searchButton.disabled=false;
 
     }
+
+}
+
+function updateProgress(solved,total,label,circle){
+    const progressDegree= (solved/total)*100;
+    circle.style.setProperty("--progress-degree", `${progressDegree}%`);
+    label.textContent=`${solved}/${total}`;
+
+}
+
+function displayUserData(parsedData){
+    const totalQues= parsedData.data.allQuestionsCount[0].count;
+      const totalEasyQues= parsedData.data.allQuestionsCount[1].count;
+        const totalMediumQues= parsedData.data.allQuestionsCount[2].count;
+          const totalHardQues= parsedData.data.allQuestionsCount[3].count;
+
+
+            const solvedTotalQues= parsedData.data.matchedUser.submitStats.acSubmissionNum[0].count;
+            const solvedEasyQues= parsedData.data.matchedUser.submitStats.acSubmissionNum[1].count;
+            const solvedMediumQues= parsedData.data.matchedUser.submitStats.acSubmissionNum[2].count;
+            const solvedHardQues= parsedData.data.matchedUser.submitStats.acSubmissionNum[3].count;
+
+
+            updateProgress(solvedEasyQues,totalEasyQues,easyLabel,easyProgressCircle);
+            updateProgress(solvedMediumQues,totalMediumQues,mediumLabel,mediumProgressCircle);
+
+            updateProgress(solvedHardQues,totalHardQues,hardLabel,hardProgressCircle);
+
 
 }
 
